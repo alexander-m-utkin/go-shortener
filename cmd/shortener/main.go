16fd961629f119fbbe6043094990295fc13b71f9
Alexander Utkin <utkin_a_m@goznak.ru>
2024-08-14 20:24:57 +0300

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		body := "http://localhost:8080/EwHXdJfB"
+		_, _ = w.Write([]byte(body))
+	}
+
+	if r.Method == http.MethodGet {
+		http.Redirect(w, r, "https://practicum.yandex.ru/", http.StatusTemporaryRedirect)
+	}
+
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, mainPage)
+
+	err := http.ListenAndServe(`:8080`, mux)
+
+	if err != nil {
+		panic(err)
+	}
+}
