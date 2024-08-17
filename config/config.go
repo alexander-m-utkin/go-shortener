8e7a325393180f770391a9d81a2a0e4ec480644a
Alexander Utkin <utkin_a_m@goznak.ru>
2024-08-17 14:55:06 +0300

    iter5

--- config/config.go
+++ config/config.go
@@ -1,19 +1,41 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"github.com/caarlos0/env/v6"
+	"log"
+)
+
+type Envs struct {
+	baseUrl       string `env:"BASE_URL"`
+	serverAddress string `env:"SERVER_ADDRESS"`
+}
 
 type Config struct {
-	A           string
-	B           string
-	isInitiated bool
+	ServerAddress string
+	BaseUrl       string
+	isInitiated   bool
 }
 
 func (c *Config) Init() {
 	if c.isInitiated != true {
-		flag.StringVar(&c.A, "a", "localhost:8080", "address and port to run server")
-		flag.StringVar(&c.B, "b", "http://localhost:8080", "shortLink prefix")
-
+		flag.StringVar(&c.ServerAddress, "a", "localhost:8080", "address and port to run server")
+		flag.StringVar(&c.BaseUrl, "b", "http://localhost:8080", "shortLink prefix")
 		flag.Parse()
+
+		var envs Envs
+		err := env.Parse(&envs)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if envs.serverAddress != "" {
+			c.ServerAddress = envs.serverAddress
+		}
+
+		if envs.baseUrl != "" {
+			c.BaseUrl = envs.baseUrl
+		}
 
 		c.isInitiated = true
 	}
