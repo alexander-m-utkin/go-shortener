1b5b4c7c738ffd65d7cf25c2064f0127194c6fb2
Alexander Utkin <utkin_a_m@goznak.ru>
2024-12-07 15:33:10 +0300

    iter7

--- internal/app/app.go
+++ internal/app/app.go
@@ -114,6 +114,7 @@ func PostShortenHandle(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	_, _ = w.Write(wBodyJSON)
 }
